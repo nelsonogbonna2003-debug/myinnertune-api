3dec6da8a2948375018e28a3eca1638cadf7c207
nelsonogbonna2003-debug <nelsonogbonna2003@gmail.com>
2025-08-16 15:48:56 +0200

    Create index.js

---
+++ pages/api/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh" 
+    }}>
+      <h1>🚀 MyInnerTune API + Frontend is live!</h1>
+    </div>
+  );
+}
